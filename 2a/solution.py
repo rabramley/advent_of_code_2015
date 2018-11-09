3ddf061c68f663e1c6ba712c1b750a61edb6f96f
Richard Bramley <rabramley@gmail.com>
2018-11-09 08:14:59 +0000

    Refactoring with better inbuilt functions

--- 2a/solution.py
+++ 2a/solution.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from itertools import permutations
-from functools import reduce
 
 wrapping_paper_sq_ft = 0
 
@@ -11,7 +10,7 @@ with open("input.txt", "r") as f:
         faces_dimensions = permutations(dimensions, 2)
         face_areas = [int(d[0]) * int(d[1]) for d in faces_dimensions]
         spare = sorted(face_areas)[0]
-        total_area = reduce(lambda x, y: x+y, [spare, *face_areas])
+        total_area = sum([spare, *face_areas])
         wrapping_paper_sq_ft += total_area
 
 print(wrapping_paper_sq_ft)
--- 2b/solution.py
+++ 2b/solution.py
@@ -2,15 +2,16 @@
 
 from itertools import permutations
 from functools import reduce
+from operator import mul
 
 ribbon_length_ft = 0
 
 with open("input.txt", "r") as f:
     for d in f:
         dimensions = [int(x) for x in d.strip().split('x')]
-        bow_length = reduce(lambda x, y: x*y, dimensions)
+        bow_length = reduce(mul, dimensions)
         shortest_edges = sorted(dimensions)[0:2]
-        shortest_distance_around_sides = reduce(lambda x, y: x+y, shortest_edges) * 2
+        shortest_distance_around_sides = sum(shortest_edges) * 2
         ribbon_length_ft += bow_length + shortest_distance_around_sides
 
 print(ribbon_length_ft)
